2114116bb5b43e1be830242fff39f8d0e0b1b40c
Yungyuen Klaymala <yungyuen.k@onetoonecontacts.com>
2024-05-10 11:14:19 +0700

    -a

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
 
 app.get("/",(req,res)=>{
-    res.render("API Working")
+    res.send("API Working")
 })
 
 app.listen(port,()=>{
